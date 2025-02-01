f8e7ac0fd7e1bd87ef74501513c1eac0a48ab1dd
sh1xzyy <alekslungol@gmail.com>
2025-02-01 20:23:08 +0300

    added scroll to top btn

--- js/main.js
+++ js/main.js
@@ -22,3 +22,21 @@ document.getElementById('open-mobile-menu').addEventListener('click', () => {
 document.getElementById('close-mobile-btn').addEventListener('click', () => {
 	document.getElementById('mobile-menu').classList.remove('is-open')
 })
+// =========== Scroll To Top ===========
+const scrollToTop = document.getElementById('scroll__to-top')
+window.onscroll = function () {
+	if (
+		document.body.scrollTop > 500 ||
+		document.documentElement.scrollTop > 500
+	) {
+		scrollToTop.classList.add('is-active')
+	} else {
+		scrollToTop.classList.remove('is-active')
+	}
+}
+scrollToTop.addEventListener('click', () => {
+	window.scrollTo({
+		top: 0,
+		behavior: 'smooth',
+	})
+})
